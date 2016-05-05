b8d49c229b84ce4e20db7c9f117ec4b3c797d61f
litianyu <lity@xueqiu.com>
2016-05-05 18:34:23 +0800

    seo test

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+
+app.use(require('prerender-node').set('prerenderToken', 'f19Nv33JiyRHohMLi1uV'));
+
 app.use('/static', express.static(__dirname + '/dist/static'));
 
 app.get('/', function (req, res) {
