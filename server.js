3a790cc6d0a61423dd439851c5b1c9ab116b8a63
litianyu <lity@xueqiu.com>
2016-05-18 18:01:50 +0800

    index file

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(require('prerender-node').set('prerenderToken', 'f19Nv33JiyRHohMLi1uV'))
 
 app.use('/static', express.static(__dirname + '/dist/static'));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 app.listen(3001, function () {
   console.log('Example app listening on port 3001!');
 });
